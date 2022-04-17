b010a4691f0a7b59915cb74912d6c7c7b2e336e1
artur <i@aderbin.ru>
2022-04-17 11:43:11 +0500

    init

---
+++ no_webpack/src/js/app.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
